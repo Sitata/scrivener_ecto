d86c8f8333cf79a4bae9c8641872ce91225769fe
Adam St. John <astjohn@gmail.com>
2019-09-24 20:27:37 -0400

    Introduce fix for nested joins.

--- lib/scrivener/paginater/ecto/query.ex
+++ lib/scrivener/paginater/ecto/query.ex
@@ -5,6 +5,64 @@ defimpl Scrivener.Paginater, for: Ecto.Query do
 
   @moduledoc false
 
+  @spec paginate(Ecto.Query.t, Scrivener.Config.t) :: Scrivener.Page.t
+  def paginate(%{joins: joins, group_bys: group_bys} = query,
+    %Config{
+      page_size: page_size,
+      page_number: page_number,
+      module: repo,
+      caller: _caller,
+      options: options})
+    when is_list(joins) and length(joins) > 0 and is_list(group_bys) and group_bys == [] do
+
+    # paginate when joins are present in query
+    offset = calc_offset(page_size, page_number, options)
+
+    # determine the ids of the entries we want
+    stripped_query = query
+                     |> exclude(:select)
+                     |> exclude(:preload)
+                     |> exclude(:group_by)
+
+    # The group_by is needed to de-duplicate. "distinct" doesnt work
+    # because the postgres driver adds the distinct column to the order_by
+    # clause, which destroys the original ordering of the query.
+    ids = stripped_query
+          |> select([x], {x.id})
+          |> group_by([x], x.id)
+          |> offset(^offset)
+          |> limit(^page_size)
+          |> repo.all()
+
+    # De-group the ids and find entities
+    as_ungrouped_ids = Enum.map(ids, fn {str_id} -> str_id end)
+
+    entries = query
+               |> where([x], x.id in ^as_ungrouped_ids)
+               |> distinct(true)
+               |> repo.all()
+
+    # We also need the total count, so we find it out
+    count = stripped_query
+            |> exclude(:order_by)
+            |> select([x], count(x.id, :distinct))
+            |> repo.one!()
+
+    total_pages = Float.ceil(count / page_size) |> round()
+    allow_overflow_page_number = Keyword.get(options, :allow_overflow_page_number, false)
+    page_number =
+      if allow_overflow_page_number, do: page_number, else: min(total_pages, page_number)
+
+    # Then we create the Scrivener-compatible page
+    %Page{
+      page_size: page_size,
+      page_number: page_number,
+      entries: entries,
+      total_entries: count,
+      total_pages: total_pages
+    }
+  end
+
   @spec paginate(Ecto.Query.t(), Scrivener.Config.t()) :: Scrivener.Page.t()
   def paginate(query, %Config{
         page_size: page_size,
@@ -13,6 +71,7 @@ defimpl Scrivener.Paginater, for: Ecto.Query do
         caller: caller,
         options: options
       }) do
+    # paginate when joins are not present in query
     total_entries =
       Keyword.get_lazy(options, :total_entries, fn -> total_entries(query, repo, caller) end)
 
@@ -34,7 +93,7 @@ defimpl Scrivener.Paginater, for: Ecto.Query do
   defp entries(_, _, page_number, total_pages, _, _, _) when page_number > total_pages, do: []
 
   defp entries(query, repo, page_number, _, page_size, caller, options) do
-    offset = Keyword.get_lazy(options, :offset, fn -> page_size * (page_number - 1) end)
+    offset = calc_offset(page_size, page_number, options)
 
     query
     |> offset(^offset)
@@ -81,6 +140,10 @@ defimpl Scrivener.Paginater, for: Ecto.Query do
     query
     |> exclude(:select)
     |> select(count("*"))
+  end
+
+  defp calc_offset(page_size, page_number, options) do
+    Keyword.get_lazy(options, :offset, fn -> page_size * (page_number - 1) end)
   end
 
   defp count(query) do
